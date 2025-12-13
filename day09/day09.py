944483a390c35e0c720937fc38eba4612fa0a81c
Alex <alexandercastronovo@gmail.com>
2025-12-13 17:43:56 -0500

    use gpu

--- day09/day09.py
+++ day09/day09.py
@@ -2,9 +2,15 @@ import itertools
 from tqdm import tqdm
 
 import numpy as np
+import taichi as ti
 
 
 def sign(v0, v1, v2):
+    return (v0[0] - v2[0]) * (v1[1] - v2[1]) - (v1[0] - v2[0]) * (v0[1] - v2[1])
+
+
+@ti.func
+def sign_parallel(v0, v1, v2):
     return (v0[0] - v2[0]) * (v1[1] - v2[1]) - (v1[0] - v2[0]) * (v0[1] - v2[1])
 
 
@@ -12,6 +18,18 @@ def point_in_triangle(pt, v0, v1, v2):
     d0 = sign(pt, v0, v1)
     d1 = sign(pt, v1, v2)
     d2 = sign(pt, v2, v0)
+    
+    has_neg = d0 < 0 or d1 < 0 or d2 < 0
+    has_pos = d0 > 0 or d1 > 0 or d2 > 0
+    
+    return not (has_neg and has_pos)
+
+
+@ti.func
+def point_in_triangle_parallel(pt, v0, v1, v2):
+    d0 = sign_parallel(pt, v0, v1)
+    d1 = sign_parallel(pt, v1, v2)
+    d2 = sign_parallel(pt, v2, v0)
     
     has_neg = d0 < 0 or d1 < 0 or d2 < 0
     has_pos = d0 > 0 or d1 > 0 or d2 > 0
@@ -61,19 +79,29 @@ def ear_clipping(vertices: np.ndarray):
     return np.array(triangles)
 
 
-def point_in_polygon(pt, poly_verts, poly_tris):
-    for i0, i1, i2 in poly_tris:
-        v0 = poly_verts[i0]
-        v1 = poly_verts[i1]
-        v2 = poly_verts[i2]
-        
-        if point_in_triangle(pt, v0, v1, v2):
-            return True
-    
-    return False
+@ti.func
+def point_in_polygon(
+    pt: ti.types.vector(2, ti.f32),
+    poly_verts: ti.template(),
+    poly_tris: ti.template(),
+):
+    hit = False
+    for t in range(poly_tris.shape[0]):
+        i0 = poly_tris[t, 0]
+        i1 = poly_tris[t, 1]
+        i2 = poly_tris[t, 2]
+
+        v0 = ti.Vector([poly_verts[i0, 0], poly_verts[i0, 1]])
+        v1 = ti.Vector([poly_verts[i1, 0], poly_verts[i1, 1]])
+        v2 = ti.Vector([poly_verts[i2, 0], poly_verts[i2, 1]])
+
+        if point_in_triangle_parallel(pt, v0, v1, v2):
+            hit = True
+
+    return hit
 
 
-def valid_rect(r0, r1, poly_verts, poly_tris):
+def edges_of_rect(r0, r1):
     xmin, xmax = sorted((r0[0], r1[0]))
     ymin, ymax = sorted((r0[1], r1[1]))
     
@@ -86,11 +114,24 @@ def valid_rect(r0, r1, poly_verts, poly_tris):
         edges.add((xmin, y))
         edges.add((xmax, y))
     
-    for edge in edges:
+    return np.array(list(edges))
+
+
+@ti.kernel
+def valid_rect(
+    edges: ti.types.ndarray(dtype=ti.f32, ndim=2),
+    poly_verts: ti.types.ndarray(dtype=ti.f32, ndim=2),
+    poly_tris: ti.types.ndarray(dtype=ti.i32, ndim=2),
+) -> ti.i32:
+
+    result = 1  # true
+
+    for i in range(edges.shape[0]):
+        edge = ti.Vector([edges[i, 0], edges[i, 1]])
         if not point_in_polygon(edge, poly_verts, poly_tris):
-            return False
-    
-    return True
+            result = 0
+
+    return result
 
 
 def rect_area(r0, r1):
@@ -108,6 +149,13 @@ def main():
     coords = np.array(coords)
     triangles = ear_clipping(coords)
     
+    coords_ti = ti.ndarray(dtype=ti.f32, shape=coords.shape)
+    coords_ti.from_numpy(coords)
+    
+    triangles_ti = ti.ndarray(dtype=ti.int32, shape=triangles.shape)
+    triangles_ti.from_numpy(triangles)
+    
+    
     num_pairs = len(coords) * (len(coords) - 1) // 2
     
     max_area = 0
@@ -117,7 +165,11 @@ def main():
         r0 = coords[i0]
         r1 = coords[i1]
         
-        if not valid_rect(r0, r1, coords, triangles):
+        edges = edges_of_rect(r0, r1)
+        edges_ti = ti.ndarray(dtype=ti.f32, shape=edges.shape)
+        edges_ti.from_numpy(edges)
+        
+        if not valid_rect(edges_ti, coords_ti, triangles_ti):
             continue
         
         area = rect_area(r0, r1)
@@ -128,4 +180,5 @@ def main():
 
 
 if __name__ == "__main__":
+    ti.init(arch=ti.gpu)
     main()
